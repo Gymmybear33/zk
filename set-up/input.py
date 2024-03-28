97c1167965ae36afdd8dbdaff116cde9afbb77e6
Jayson <jayson.caminante@gmail.com>
2024-03-28 14:56:36 -0400

    added input

---
+++ set-up/input.py
@@ -0,0 +1,12 @@
+# factory = input("Factory: ")
+guess_num = int(input("Guess a number: "))
+
+
+# if factory == "COX1":
+#     print(factory, "belongs to OFBG")
+# else:
+#     print("nice!")
+
+total = guess_num * 10
+print(total)
+print(str(total) + "wepa")
