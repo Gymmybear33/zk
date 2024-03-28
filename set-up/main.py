ea2cba19536f38ee6f806e69cb05a3ede598b64d
Jayson <jayson.caminante@gmail.com>
2024-03-28 09:55:16 -0400

    initial commit

---
+++ set-up/main.py
@@ -0,0 +1,18 @@
+# This is a note
+
+# print("Hello there everyone learning!")
+
+# print("Item cost:")
+# Give a 20% discount
+# print(int((25*4) * .2))
+
+
+# print("Funds to send: ", 200 * 5 + 600 / 5 + 750 - 75)
+
+
+# print(57 % 3)
+# print(57 % 4)
+
+print(30 / 9)
+print(30 // 9)
+
